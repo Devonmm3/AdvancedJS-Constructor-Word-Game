5c769ab1bccdda18073c37b3bf2938acabe80766
DevonMM <Devon.m.moran@gmail.com>
2019-01-23 18:58:07 -0500

    code done

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function gameChooseTeam() {
 
 function playerGuessTeam() {
 
-    if (guesses > 0 && points < 5) {
+    if (guess > 0 && points < 5) {
         console.log(chosenTeam.display());
 
         inquirer.prompt([{
@@ -37,6 +37,83 @@ function playerGuessTeam() {
         }]).then(function (letterGuess) {
             var guess = letterGuess.txt;
             chosenTeam.checkTheGuess(guess);
-        })
+            if (randomTeam.lowerCase().indexOf(guess.lowerCase()) === -1) {
+                guess--;
+                console.log(chalkStencil.purple("WRONG! " + guess + " chances left!"))
+            } else {
+                if (points < 5) {
+                    console.log(chalkStencil.red("Right on!"))
+                }
+            }
+            if (randomTeam === chosenTeam.display()) {
+                console.log(chosenTeam.display());
+                guess = 10;
+                points++;
+                if (points < 5) {
+                    console.log(chalkStencil.red("YAS YAS YAS! Next Team Woohoo!"));
+                    gameChooseTeam();
+                } else {
+                    winGame();
+                }
+            }
+            if (guess === 0) {
+                loseGame();
+            }
+            playerGuessTeam();
+        });
     }
 }
+
+function loseGame() {
+    console.log(chalkStencil.purple("GAME OVER BOO!"));
+    inquirer.prompt([{
+            name: "confirm",
+            type: "confirm",
+            message: "Play again?",
+            default: true
+        }])
+        .then(function (inquirerResponse) {
+            if (inquirerResponse.confirm) {
+                guess = 10;
+                points = 0;
+                gameChooseTeam();
+                playerGuessTeam();
+            } else {
+                console.log(chalkStencil.red("See you next season!"));
+                process.exit();
+            }
+        })
+}
+
+function winGame() {
+    sansSerif("TOUCHDOWN!!!!!!!!!!!!!!", function (err, data) {
+        if (err) {
+            console.log("Whoops! There is an error");
+            console.dir(err);
+            return;
+        }
+        console.log(data)
+    })
+    inquirer.prompt([{
+            name: "Confirm",
+            type: "confirm",
+            message: "Want to play again?",
+            default: true
+        }])
+        .then(function (inquirerResponse) {
+            if (inquirerResponse.comfirm) {
+                guess = 10;
+                points = 0;
+                gameChooseTeam();
+                playerGuessTeam();
+            } else {
+                console.log(chalkStencil.purple("bye bye!"))
+                process.exit();
+            }
+        })
+
+}
+
+startGame();
+gameChooseTeam();
+playerGuessTeam();
